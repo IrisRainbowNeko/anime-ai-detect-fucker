fa269918c4e87c99a77ae09d18642676ea4321ed
dzy7e <228218809@qq.com>
2023-01-16 19:14:26 +0800

    update

--- setup.py
+++ setup.py
@@ -5,5 +5,7 @@ setup(
     version='1.0',
     description='anime-ai-detect-attacker',
     author='7eu7d7',
+    author_email='',
     packages=['aai_attacker'],  # same as name
+    install_requires=[],  # external packages as dependencies
 )
